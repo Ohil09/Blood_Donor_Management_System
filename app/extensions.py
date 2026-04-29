fb36e63d3a98b0a822a4d7ef0d0e53a795dec7b9
Ohil <ohil.09@outlook.com>
2026-04-29 12:45:55 +0530

    Removed duplication

--- app/extensions.py
+++ app/extensions.py
@@ -12,18 +12,6 @@ login_manager.login_message = "Please log in to access this page."
 login_manager.login_message_category = "warning"
 
 
-from flask_login import LoginManager
-from flask_mail import Mail
-from flask_wtf import CSRFProtect
-
-login_manager = LoginManager()
-mail = Mail()
-csrf = CSRFProtect()
-
-login_manager.login_view = "auth.login"
-login_manager.login_message = "Please log in to access this page."
-login_manager.login_message_category = "warning"
-
 @login_manager.user_loader
 def load_user(user_id):
     from app.models.user import User
